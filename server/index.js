366329c0000eca26e1daa76b10b6e990075806db
rohitbakoliya <bakoliyarohit00@gmail.com>
2021-02-11 22:40:09 +0530

    chore: basic express setup

--- server/index.js
+++ server/index.js
@@ -1,1 +1,36 @@
-console.log('hello world');
+const express = require('express');
+const app = express();
+
+const helmet = require('helmet');
+const cors = require('cors');
+const morgan = require('morgan');
+const compression = require('compression');
+
+require('dotenv').config();
+
+const PORT = process.env.PORT || 5000;
+
+// express setting
+app.set('env', process.env.NODE_ENV);
+
+// middlewares
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '10kb' }));
+app.use(helmet()); // security headers
+app.use(morgan('dev'));
+app.use(
+  cors({
+    credentials: true,
+  })
+);
+app.use(compression()); // compress response bodies
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/`);
+});
+
+app.use('/', (req, res) => res.send('hello world'));
+
+app.use('/api/*', (req, res) => {
+  res.json({ error: 'No endpoint found!' });
+});
