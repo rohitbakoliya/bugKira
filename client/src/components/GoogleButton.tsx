482c96fdcd72158a29de3833f3af55b353f89a56
rohitbakoliya <bakoliyarohit00@gmail.com>
2021-04-11 07:54:11 +0530

    fix: post msg during oauth

--- client/src/components/GoogleButton.tsx
+++ client/src/components/GoogleButton.tsx
@@ -1,8 +1,7 @@
-import React, { useCallback, useEffect } from 'react';
+import React from 'react';
 import styled from 'styled-components/macro';
 import googleLogo from 'assets/svg/google.svg';
 import { Button } from '@bug-ui';
-import { SERVER_URL } from 'config/siteUrls';
 import { useDispatch } from 'react-redux';
 import { checkAuth } from 'store/ducks';
 import toast from 'react-hot-toast';
@@ -26,31 +25,30 @@ const GoogleButton: React.FC<Props> = props => {
   const dispatch = useDispatch();
 
   const authEndPoint = `/api/user/auth/google`;
-  const authURL = SERVER_URL + authEndPoint;
 
-  const succssEventListener = useCallback(() => {
+  const initOAuthWindow = () => {
+    let url = process.env.NODE_ENV === 'development' ? 'localhost:5000' : window.location.host;
+
+    window.open(
+      `${window.location.protocol}//${url}${authEndPoint}`,
+      '__blank',
+      'width=500&height=800'
+    );
     window.addEventListener('message', event => {
-      if (event.origin === SERVER_URL && event.data === 'success') {
+      if (event.data === 'success') {
         dispatch(checkAuth())
-          .then(() => toast.success('Logged In successfully!'))
-          .catch((err: string) => toast.error(err));
+          .then(() => {
+            toast.success('Login success');
+          })
+          .catch((err: string) => {
+            toast.error(err);
+          });
       }
     });
-  }, [dispatch]);
-
-  const initOAuth = () => {
-    window.open(authURL, '__blank', 'width=500&height=800');
-    succssEventListener();
   };
 
-  useEffect(() => {
-    return () => {
-      window.removeEventListener('message', succssEventListener);
-    };
-  }, [succssEventListener]);
-
   return (
-    <StyledGB onClick={initOAuth} {...props}>
+    <StyledGB onClick={initOAuthWindow} {...props}>
       <img src={googleLogo} alt='google logo' /> Continue with Google
     </StyledGB>
   );
